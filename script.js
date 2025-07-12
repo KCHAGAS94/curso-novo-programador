389e7d1902fbfbd0628e98d2f0c61ba41366fb5a
kchagas94 <kenedychagas@gmail.com>
2025-07-12 00:40:39 -0300

    add-link-e-typescript

--- script.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('quizForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio do formulário
-    alert('Respostas enviadas! Verifique o seu progresso.');
-});
